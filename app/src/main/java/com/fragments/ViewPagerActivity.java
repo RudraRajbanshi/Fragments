a1e2fbc7933f643f9526a6e7054644c0387ea114
RudraRajbanshi <rudra.raj.716@gmail.com>
2019-04-22 11:20:31 +0545

    Fragments completed

---
+++ app/src/main/java/com/fragments/ViewPagerActivity.java
@@ -0,0 +1,31 @@
+package com.fragments;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import adapter.ViewPagerAdapter;
+import fragments.FirstFragment;
+import fragments.SecondFragment;
+
+public class ViewPagerActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabId);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FirstFragment(),"Sum");
+        adapter.addFragment(new SecondFragment(),"Area of Circle");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
